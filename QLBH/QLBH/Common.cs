513fcaf55cd096c39a7ac1a38de7ba0a1ebc6f8b
Phạm Thanh Sơn <vandertyson117@gmail.com>
2016-03-19 16:03:18 +0700

    fff

---
+++ QLBH/QLBH/Common.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Drawing.Imaging;
+using LibraryApi;
+using System.Drawing;
+
+
+namespace QLBH
+{
+    class Common
+    {
+        public static Image get_image(string link)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(link);
+
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    return Image.FromStream(mem);
+                }
+
+            }
+        }
+    }
+}
